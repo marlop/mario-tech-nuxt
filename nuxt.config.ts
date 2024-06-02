eb4cd129dee67773944a86d1d3df26b6595f0f08
marlop <26493202+marlop@users.noreply.github.com>
2024-06-01 23:24:04 -0700

    add meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Label Printing",
+      meta: [
+        { charset: "utf-8" },
+        {
+          name: "viewport",
+          content:
+            "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
+        },
+      ],
+    },
+  },
+});
